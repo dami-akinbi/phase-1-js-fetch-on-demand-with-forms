90f35d592d143596d12cacd98d1f85f123e13af4
damilolaakinbi <akinbidamilola074@gmail.com>
2023-01-18 09:29:15 +0100

    Completed.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 const init = () => {
-  
-}
+  const form = document.querySelector("form");
 
-document.addEventListener('DOMContentLoaded', init);
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const userInput = document.querySelector("#searchByID");
+
+    fetch(`http://localhost:3000/movies/${userInput.value}`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+
+        const h4 = document.querySelector("#movieDetails h4");
+        const p = document.querySelector("#movieDetails p");
+        h4.innerText = `Title: ${data.title}`;
+        p.innerText = `Summary: ${data.summary}`;
+        // findById(data);
+      });
+
+    // function findById(arrayOfData) {
+    //   for (member of arrayOfData)
+    //     if (member.id === userInput) console.log(member);
+    // }
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
